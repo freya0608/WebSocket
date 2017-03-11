7498fe49b1b68b7d0b67e0b2ae6bc9e02c657f6b
freya0608 <freya001@yeah.net>
2017-03-12 00:40:32 +0800

    server

---
+++ serve/server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by freya on 2017/3/12.
+ */
+var events = require('events');
+var http = require('http');
+var crypto = require('crypto');
+var util = require('util');
+var opcodes = {
+    
+}
